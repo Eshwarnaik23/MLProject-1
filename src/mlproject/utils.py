a712edb7b084cb48fa4eade742399fc252d7894e
Eshwar <eshwartejavathu2023@gmail.com>
2025-03-03 20:06:57 +0530

    model trainer

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging 
 from urllib.parse import quote
-#from sklearn.model_selection import GridSearchCV
-#from sklearn.metrics import r2_score
-#from src.mlproject.components import model_tranier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 
 import pickle
@@ -57,4 +57,37 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-
+def evalauate_models(X_train,y_train,X_test,y_test,param, models):
+    ''' what this model does?
+    ANS: Take all the algorithms and also the parameters and will fit
+    and compute the r2 socre on train and test socore values will be put in dictonaries
+    as key and value pair and return in the report.
+    '''
+    try:
+        report={}
+       
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+           
+            gs= GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+           
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+           
+            #model.fit(X_train,y_train)
+            #Train model
+           
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+           
+            train_model_score = r2_score(y_train,y_train_pred)
+            
+            test_model_score=r2_score(y_test,y_test_pred)
+           
+            report[list(models.keys())[i]]=test_model_score
+           
+        return report
+    except Exception as e:
+        raise CustomException( e,sys)
